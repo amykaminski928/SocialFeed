d37b02b0e044c7b66443885bf9c0b068c6b1bd97
Amy Kaminski <amykaminski928@gmail.com>
2023-03-19 12:19:58 -0500

    Planned changes = saving this version before reworking code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
-// import AddPostForm from './Components/CreatePost/CreatePost';
+import AddPostForm from './Components/CreatePost/CreatePost';
 import PostCard from './Components/Post/Post';
 
 
 function App() {
-  // const [posts, setPosts] = useState([{name: "Amy Kaminski", post: "I am so excited to learn to code."}, {name: "Oliver Kaminski", post: "Me too nerding out is fun."}])
-  let posts = [{Username: "Amy Kaminski", post: "I am so excited to learn to code."}, {Username: "Oliver Kaminski", post: "Me too nerding out is fun."}]
-  // function addNewPost(post){
-  //   let tempPosts = [...posts, post];
-  //   setPosts(tempPosts);
-  // }
-    return (
-   
+
+  const [posts, setPosts] = useState([{name: "Amy Kaminski", post: "I am so excited to learn to code."}, {name: "Oliver Kaminski", post: "Me too nerding out is fun."}])
+  // let posts = [{Username: "Amy Kaminski", post: "I am so excited to learn to code."}, {Username: "Oliver Kaminski", post: "Me too nerding out is fun."}]
+  function addNewPost(post){
+    let tempPosts = [...posts, post];
+    setPosts(tempPosts);
+  }
+
+    return (   
     <div className='container-fluid'>
       <div className='row'>
         <h3 style={{margin:'1em'}}>Social
         <small className= 'text-muted'>FEED</small></h3>
-        <div className='border-box'>
-          <form>
-            <h3>Name:</h3>
-            <h3>Post:</h3>
-          </form>
-          {/* <div>{posts}</div> */}
-              {/* <AddPostForm addPostProperty={addNewPost}/>     */}
+        <div className= 'form-inline justify-content-center' >
+          <div className='border-box'>
+           <AddPostForm addPostProperty={addNewPost}/>    
+          </div>
         </div>
         <div className='border-box'>
-          <div>{PostCard}        
+          <div>     
             <div className='post-content'>
-              <DisplayPosts parentPosts = {posts}/>
+              <DisplayPosts parentPosts = {PostCard(tempPosts)}/>
             </div>
           </div>
         </div>
--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,36 +1,37 @@
-// import React, { useState } from 'react';
-// const AddPostForm = (props) => {
+import React, { useState } from 'react';
 
-//     const [userName, setUserName] = useState('');
-//     const [post, setPost] = useState('');
+const AddPostForm = (props) => {
+
+    const [userName, setUserName] = useState('');
+    const [post, setPost] = useState('');
     
-//     function handleSubmit(event) {
-//         event.preventDefault();
-//         let newEntry ={
-//             name: (userName),
-//             post: (post)
-//         };
-//         console.log(newEntry);
-//         props.addPostProperty(newEntry)
-//     }
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost =[{
+            name: (userName),
+            post: (post)
+        }];
+        console.log(newPost);
+        props.addPostProperty(newPost)
+    }
 
 
-//     return ( 
-//         <form onSubmit={handleSubmit} className="form-grid">
-//             <div className='form-group'>
-//                 <label>Name</label>
-//                 <input type = 'text' className="form-control" value={userName} onChange={(event) => setUserName(event.target.value)}/>
-//             </div>
-//             <div className='form-group'>
-//                 <label>Post</label>
-//                 <input type= 'text' className="form-control" value={post} onChange={(event) => setPost(event.target.value)}/>
-//             </div>
-//             <button type= 'submit' className="btn btn-outline-primary" style={{'margin-top': '1em'}}>CREATE</button>
-//         </form>
-//      );
-// }
+    return ( 
+        <form onSubmit={handleSubmit} className="form-grid">
+            <div className='form-group'>
+                <label>Name</label>
+                <input type = 'text' className="form-control" value={userName} onChange={(event) => setUserName(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Share your kind thoughts here:</label>
+                <input type= 'text' className="form-control" value={post} onChange={(event) => setPost(event.target.value)}/>
+            </div>
+            <button type= 'submit' className="btn btn-outline-primary" style={{'margin-top': '1em'}}>SHARE</button>
+        </form>
+     );
+}
  
-// export default AddPostForm;
+export default AddPostForm;
 
 
  
